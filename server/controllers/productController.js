8d607a4f86bc5dd375767191c064a029e3634067
Maksym <uvarovskyi.m.keykey@gmail.com>
2024-11-06 14:45:28 +0200

    update searchbar

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -19,7 +19,11 @@ export const getProductsByCategory = async (req, res) => {
 
 export const getAllProducts = async (req, res) => {
     try {
-        const products = await Product.find({});
+        const { search } = req.query; 
+
+        const query =  { title: { $regex: `(^|\\s)${search}`, $options: 'i' } }
+
+        const products = await Product.find(query);
         res.json(products);
     } catch (error) {
         res.status(500).json({ message: 'Помилка отримання даних', error });
